a24344c183021ee6d86d020c559e9166e83aa887
Ваня Городничев <lennygames172@gmail.com>
2025-06-09 18:47:41 +0500

    Финал

--- src/api/destination-api.js
+++ src/api/destination-api.js
@@ -1,7 +1,7 @@
 import ApiService from '../framework/api-service.js';
 
 export default class DestinationsApi extends ApiService {
-  get allDestinations() {
+  get destinations() {
     return this._load({ url: 'destinations' }).then(ApiService.parseResponse);
   }
 }
